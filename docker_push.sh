566f4b5d60b80f9280720c7fe5b5b129fc92893b
noneedrelax <noneedrelax@gmail.com>
2019-09-10 22:21:44 +0800

    updated docker

--- docker_push.sh
+++ docker_push.sh
@@ -1,7 +1,7 @@
 echo "name space is ${myNs}, SHA is  ${SHA}"
 mkdir -p ./target/
 cp $HOME/upload/testweb.war ./target/
-docker build -t ${myNs}/testweb:latest -t ${myNs}/multi-client:${SHA} ./
+docker build -t ${myNs}/testweb:latest -t ${myNs}/testweb:${SHA} ./
  
 # update latest version
 docker push ${myNs}/testweb:latest
